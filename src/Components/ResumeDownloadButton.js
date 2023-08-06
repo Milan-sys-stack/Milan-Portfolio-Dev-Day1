2402798c6738c78c3c9642a81dec9a3089007698
Milan Ghimire <milanghimire656@gmail.com>
2023-08-06 15:45:23 +1000

    Milan-Portfolio-Dev-Day1

--- src/Components/ResumeDownloadButton.js
+++ src/Components/ResumeDownloadButton.js
@@ -5,7 +5,7 @@ import React from 'react';
 export const ResumeDownloadButton = () => {
   const downloadResume = () => {
     const downloadLink = document.createElement('a');
-    downloadLink.href = '#public/MilanResume.pdf'; // Path to the resume file in the public folder
+    downloadLink.href = 'http://localhost:3000/MilanResume.pdf'; // Path to the resume file in the public folder
     downloadLink.download = 'MilanResume.pdf'; // Set the desired filename for download
     document.body.appendChild(downloadLink);
     downloadLink.click();
